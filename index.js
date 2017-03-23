bbd6869668c0afa7c7028649fa79496699d76903
graysoft <zhangchao@neutrey.com>
2017-03-23 16:02:47 +0800

    add express and dev middleware.

--- index.js
+++ index.js
@@ -1,59 +1,27 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
 
-types = {
-  "css": "text/css",
-  "gif": "image/gif",
-  "html": "text/html",
-  "ico": "image/x-icon",
-  "jpeg": "image/jpeg",
-  "jpg": "image/jpeg",
-  "js": "text/javascript",
-  "json": "application/json",
-  "pdf": "application/pdf",
-  "png": "image/png",
-  "svg": "image/svg+xml",
-  "swf": "application/x-shockwave-flash",
-  "tiff": "image/tiff",
-  "txt": "text/plain",
-  "wav": "audio/x-wav",
-  "wma": "audio/x-ms-wma",
-  "wmv": "video/x-ms-wmv",
-  "xml": "text/xml"
-};
+var express = require('express');
+var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
+var config = require('./config/webpack.config');
 
-var server = http.createServer(function (req, res) {
-  var pathname = url.parse(req.url).pathname;
+var app = express();
 
-  if(pathname.endsWith('/'))
-    pathname += 'index.html';
+compiler = webpack(config);
 
-  var realPath = path.join(__dirname, decodeURI(pathname));
-  fs.exists(realPath, function (exists) {
-    if(!exists) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('File not found:', realPath);
-    } else {
-      fs.readFile(realPath, 'binary', function (err, file) {
-        if (err) {
-          res.writeHead(500, {'Content-Type': 'text/plain'});
-          res.end(err);
-        } else {
-          var ext = path.extname(realPath);
-          ext = ext ? ext.slice(1) : 'unknown';
-          var contentType = types[ext] || 'text/plain';
+app.use(webpackMiddleware(compiler, {
+  noInfo: false,
+  stats: {
+    colors: true,
+    chunks: false,
+    children: false,
+    chunkModules: false
+  },
+}));
 
-          res.writeHead(200, {'Content-Type': contentType});
-          res.write(file, 'binary');
-          res.end();
-        }
-      });
-    }
-  });
-});
-
-server.listen(8000, function (err, res) {
-  console.log('Server is running at port: 8000.');
+app.listen(8090, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('listening at port: 8090');
+  }
 });
